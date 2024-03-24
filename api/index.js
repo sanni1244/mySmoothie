dfcc46d4145930129be0c2cfb63fc2898eebe667
Opeyemi <opeyemi.sanni180591052@st.lasu.edu.ng>
2024-03-24 17:28:14 +0100

    done

--- api/index.js
+++ api/index.js
@@ -1,10 +1,60 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
 
-app.use("/", (req, res) => {
-    res.send("Express is here");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json()); // Parse JSON request bodies
+
+const PORT = process.env.PORT || 5000;
+
+app.post('/api', (req, res) => {
+  const { subject, name, orderName, price, message, myNumber } = req.body;
+
+  if (!subject || !name || !orderName || !price || !message || !myNumber) {
+    return res.status(400).json({ error: 'Missing required fields in request body' });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'sanni.ope.0@gmail.com',
+      pass: 'etlr mgmk lalf hwjm'
+    }
+  });
+
+  const mailOptions = {
+    from: 'sanni.ope.0@gmail.com',
+    to: 'sanni.ope.0@gmail.com',
+    subject: subject,
+    html: `<h2>You have a new order 🥳🥳🥳</h2>
+      <br/><br/>
+      <h4>Your order is from ${name}</h4>
+      <p>Here is the order</p>
+      <ul>
+      <li> Order:  ${orderName} </li>
+      <li> Price:  ${price} </li>
+      </ul>
+      <p><b>Comments</b></p>
+
+      <p>${message}</p>
+
+      Phone Number: <a href="tel:${myNumber}">${myNumber}</a>
+    `
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email: ' + error.message);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent successfully');
+    }
+  });
 });
 
-app.listen(5000, console.log("Server ready on port 5000."));
-
-
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
