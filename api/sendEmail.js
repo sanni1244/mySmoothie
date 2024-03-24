9e6e703c034be538255063212b856493cb34536b
Opeyemi <opeyemi.sanni180591052@st.lasu.edu.ng>
2024-03-24 10:57:40 +0100

    a,d

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
   const { subject, name, orderName, price, message, myNumber } = req.body;
  
   const transporter = nodemailer.createTransport({
