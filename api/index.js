e481d00843b2ee62c7c8145d99e6fee82b5fa31f
Opeyemi <astro8va@gmail.com>
2025-03-31 12:21:20 +0100

    check

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.post('/api', (req, res) => {
     service: 'gmail',
     auth: {
       user: 'sanni.ope.0@gmail.com',
-      pass: env.PASS_KEY // Use environment variable for password
+      pass: env.PASS_KEY
     }
   });
 
